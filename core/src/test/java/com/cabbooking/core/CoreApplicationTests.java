f4d8ade51fe3131697e9724cddec15873797719a
amolb <amolb@amolbs-MacBook-Air.local>
2024-05-24 16:48:04 +0400

    Basic setup

---
+++ core/src/test/java/com/cabbooking/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabbooking.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
